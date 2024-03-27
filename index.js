a914eaca1edb973f91dc9bfae4a0206520351779
Edward Koikai <edwardkoikai@gmail.com>
2024-03-27 04:58:59 +0300

    modifies index,js to add functionality

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Edward Hawkins",
+    streetAddress: "Moscow 14th Avenue"
+}
+function updateEmployeeWithKeyAndValue(employee, key , value ){
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+ const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Caesar");
+
+ function deleteFromEmployeeByKey(employee, key){
+    const postEmployee = {...employee};
+    delete postEmployee[key];
+    return postEmployee
+ }
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name ;
+    return employee
+ }
+
+
